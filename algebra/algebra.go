d2ddd519050a463cb56b38547107935429a2d6cd
AnvilDev <andres.ernesto.valdes@gmail.com>
2018-04-12 19:28:46 -0400

    Added documentation

--- algebra/algebra.go
+++ algebra/algebra.go
@@ -4,6 +4,10 @@ import (
 	"sync"
 )
 
+// This function adds two matrices in the form
+// [ a b ] + [ e f ] = [ a+e b+f ]
+// [ c d ]   [ g h ]   [ c+g d+h ]
+// It does so parallelized row-by-row
 func addMatrices(matrixA [][]float32, matrixB [][]float32) [][]float32 {
 	if matrixA == nil || matrixB == nil {
 		return nil
@@ -14,23 +18,30 @@ func addMatrices(matrixA [][]float32, matrixB [][]float32) [][]float32 {
 	bNumRows := len(matrixB)
 	bNumCols := len(matrixB[0])
 
+	// Make sure the matrices are of equal dimensions
 	if aNumCols != bNumCols || aNumRows != bNumRows {
 		return nil
 	}
 
+	// Instantiate the goroutine pool.
 	var wg sync.WaitGroup
+
+	// Instantiate a matrix to return
 	result := make([][]float32, aNumRows, aNumCols)
 
+	// Add the number of columns to the goroutine pool
 	wg.Add(aNumRows)
 	for i := range matrixA {
 		go addRow(i, matrixA, matrixB, result, &wg)
 	}
+	// Wait until all goroutines have finished before returning
 	wg.Wait()
 
 	return result
 
 }
 
+// Add two rows of two matrices together in a goroutine
 func addRow(irow int, matrixA [][]float32, matrixB [][]float32, result [][]float32, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for i := range matrixA[irow] {
@@ -38,6 +49,9 @@ func addRow(irow int, matrixA [][]float32, matrixB [][]float32, result [][]float
 	}
 }
 
+// Multiply a matrix by a scalar in the form:
+// g[ a b ] = [ ga gb ]
+//  [ c d ]   [ gc gd ]
 func scalarMultiply(scalar float32, matrix [][]float32) {
 	if matrix == nil {
 		return
@@ -56,6 +70,7 @@ func scalarMultiply(scalar float32, matrix [][]float32) {
 	}
 }
 
+// Multiply a row of a matrix in a goroutine
 func scaleRow(irow int, scalar float32, matrix [][]float32, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for i := range matrix[irow] {
