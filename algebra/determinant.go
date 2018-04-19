d9c8510ba5d369235baefff91d4deabef758dbbf
Alejandro <alejandro.perez.developer@gmail.com>
2018-04-19 14:24:17 -0400

    Working on functionality to parelallize matrix determinant

--- algebra/determinant.go
+++ algebra/determinant.go
@@ -1,6 +1,11 @@
 package algebra
 
-import "errors"
+import (
+	"errors"
+	"sync"
+	"fmt"
+	"math"
+)
 
 // GetDeterminant returns the determinant of an n x n matrix
 func GetDeterminant(matrix [][]float32) (float32, error) {
@@ -20,6 +25,64 @@ func GetDeterminant(matrix [][]float32) (float32, error) {
 		return matrix[0][0]*matrix[1][1]*matrix[2][2] - matrix[0][0]*matrix[1][2]*matrix[2][1] - matrix[0][1]*matrix[1][0]*matrix[2][2] + matrix[0][1]*matrix[1][2]*matrix[2][0] + matrix[0][2]*matrix[1][0]*matrix[2][1] - matrix[0][2]*matrix[1][1]*matrix[2][0], nil
 	}
 
-	//TODO: The non-trivial, recursive, parallelized determinant.
-	return 0, errors.New("dev: this return is dummy and has not been finalized")
+	// Instantiate the goroutine pool.
+	var wg sync.WaitGroup
+
+
+	// Instantiate a matrix to return
+	determinants := make([]float32, len(matrix))
+
+	for i := range matrix {
+		wg.Add(1)
+		go func() {
+			innerMatrix := extractInnerMatrixIgnoringGivenRowAndCol(0, i, matrix)
+			det, err := GetDeterminant(innerMatrix)
+
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				determinants[i] = det * float32(math.Pow(float64(-1), float64(i)))
+			}
+			wg.Done()
+		}()
+	}
+
+	// Wait until all goroutines have finished before returning
+	wg.Wait()
+
+	var response float32 = 0
+
+	for i := range determinants {
+		response += determinants[i] * matrix[0][i]
+	}
+
+	return response, nil
+}
+
+func extractInnerMatrixIgnoringGivenRowAndCol(elementRow int, elementCol int, matrix [][]float32) [][]float32 {
+	innerMatrix := make([][]float32, len(matrix) - 1)
+
+	for row := range matrix {
+		if(elementRow == row) {
+			continue
+		}
+
+		realRow := row
+		if(elementRow <= row) { realRow = row - 1 }
+
+		innerMatrix[realRow] = make([]float32, len(matrix) - 1)
+
+		for col := range matrix[row] {
+			if(elementCol == col) {
+				continue
+			}
+
+			realCol := col
+			if(elementCol <= col) { realCol = col - 1 }
+
+			innerMatrix[realRow][realCol] = matrix[row][col]
+		}
+	}
+
+	return innerMatrix
 }
--- subcinv.go
+++ subcinv.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/SubcubicInversion/implementation/utils"
 	"github.com/SubcubicInversion/implementation/timer"
+	"github.com/SubcubicInversion/implementation/algebra"
 )
 
 func main() {
@@ -12,10 +13,25 @@ func main() {
 	timer.StartTimer(&executionTime, "Execution Test")
 
 	fmt.Println(utils.IsPowerOfTwo(1))
-	a := utils.GenerateSquaredMatrix(8)
+	a := utils.GenerateSquaredMatrix(5)
+	matrix := [][]float32{
+		{2, 7, 3, 5},
+		{5, 1, 5, 3},
+		{9, 2, 4, 7},
+		{1, 4, 6, 2},
+	}
+
+	det, err := algebra.GetDeterminant(matrix)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(det)
+	}
+
+
 	fmt.Println(a)
 
-	timer.StopTimer(&executionTime, "Execution Test")
 	fmt.Printf("Elapsed Time: %f seconds", timer.ElapsedTime(&executionTime).Seconds())
 	fmt.Println("")
 }
