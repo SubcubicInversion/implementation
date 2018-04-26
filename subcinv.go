b66ef28b227ee9191b696694a45e20ff6eaeaf55
AnvilDev <andres.ernesto.valdes@gmail.com>
2018-04-26 06:17:38 -0400

    The thing now times.

--- subcinv.go
+++ subcinv.go
@@ -2,22 +2,20 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/SubcubicInversion/implementation/utils"
-
-	"github.com/SubcubicInversion/implementation/timer"
 
 	"github.com/SubcubicInversion/implementation/algebra"
 )
 
 func main() {
-	var executionTime timer.Timer
-	timer.StartTimer(&executionTime, "Execution Test")
+	start := time.Now()
 
 	matrix1 := utils.GenerateSquareMatrix(512)
 
 	fmt.Println(algebra.InvertMatrix(matrix1))
 
-	fmt.Printf("Elapsed Time: %f seconds", timer.ElapsedTime(&executionTime).Seconds())
-	fmt.Println("")
+	elapsed := time.Since(start)
+	fmt.Printf("Elapsed Time: %s", elapsed)
 }
